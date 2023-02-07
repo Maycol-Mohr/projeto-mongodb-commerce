67f94027284ffe477e51cd36fe9931808e04966f
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-02-07 20:58:07 +0100

    tentando de 11 ate 22

---
+++ challenges/desafio17.js
@@ -0,0 +1,12 @@
+db.resumoProdutos.insertOne(
+    {
+          franquia: "McDonalds",
+          totalProdutos: db.produtos.countDocuments({}),
+    },
+    { upsert: true },
+);
+  
+db.resumoProdutos.find(
+      {},
+      { franquia: 1, totalProdutos: 1, _id: 0 }
+);
